ec0b037fe584fae867527f66090deb3336eaf9d5
Tola's Terminal <sohbaker@gmail.com>
2018-11-26 14:44:42 +0000

    user can add a to do action to their list

--- todo_list.rb
+++ todo_list.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
  get '/' do
-   "Hello, world"
-   # erb :index
+   todo = params["action_item"]
+   list_of_actions = []
+
+   list_of_actions << todo
+   erb :index, :locals => {:todo => todo, :list_of_actions => list_of_actions}
  end
