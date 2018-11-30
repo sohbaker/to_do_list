da13af78a8475e76ed8b08f5fddab2c104ff9d41
Tola's Terminal <sohbaker@gmail.com>
2018-11-30 13:29:45 +0000

    user is able to display todo list using filters

--- database.rb
+++ database.rb
@@ -1,13 +1,22 @@
 require 'pg'
 
 class RunDatabase
-  def initialize
-    PG.connect(
+  def self.run(sql)
+      begin
+    db = PG.connect(
       dbname: 'postgres',
-      host: 'localhost'
+      host: 'localhost',
+      user: 'postgres',
+      password: 'dataadmin'
     )
+      result = db.exec(sql)
+    ensure
+      db.close
+      end
+    return result
     # if db = true
     #   return "get started with to do list"
     # end
   end
+
 end
--- manage_database.rb
+++ manage_database.rb
@@ -4,40 +4,51 @@ require 'pg'
 class ManageDatabase
   def initialize
     @connect = RunDatabase.new
+    @log_number = 0
   end
 
-  def save_to_database
-    exec(@connect.save)
+  def save_to_database(data_to_save)
+    sql = data_to_save
+    RunDatabase.run(sql).first
+  end
+
+  def connect_to_database
+    RunDatabase.new
   end
 
   def add_item(item)
-    "INSERT into todo_list VALUES ('#{item}','active');"
-    save_to_database
+    @log_number = @log_number + 1
+    data = "INSERT into todo_list VALUES ('#{@log_number.to_i}','#{item}','active');"
+    save_to_database(data)
+    return @log_number
   end
 
   def edit_item(item_to_edit, change_to)
-    "UPDATE todo_list SET action = '#{change_to}' WHERE action = '#{item_to_edit}';"
-    save_to_database
+    data = "UPDATE todo_list SET action = '#{change_to}' WHERE action = '#{item_to_edit}';"
+    save_to_database(data)
   end
 
   def mark_complete(completed_action)
-    "UPDATE todo_list SET status = 'complete' WHERE action = '#{completed_action}';"
-    save_to_database
+    data = "UPDATE todo_list SET status = 'complete' WHERE action = '#{completed_action}';"
+    save_to_database(data)
   end
 
   def view_full_list
-    display_list = @connect.exec("SELECT * from todo_list")
-    return display_list.values
+    sql = "SELECT * from todo_list;"
+    display_list = RunDatabase.run(sql).values
+    display_list
   end
 
   def view_active_list
-    display_list = @connect.exec("SELECT * from todo_list WHERE status = 'active';")
-    return display_list.values
+    sql = "SELECT * from todo_list WHERE status = 'active';"
+    display_list = RunDatabase.run(sql).values #returns only the first value in the database
+    display_list
   end
 
   def view_completed_list
-    display_list = @connect.exec("SELECT * from todo_list WHERE status = 'complete';")
-    return display_list.values
+    sql = "SELECT * from todo_list WHERE status = 'complete';"
+    display_list = RunDatabase.run(sql).values #returns only the first value in the database
+    display_list
   end
 
   def exit
