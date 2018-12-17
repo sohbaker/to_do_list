b03effc6fafb0783783a55c08a32401853be93a5
Tola's Terminal <sohbaker@gmail.com>
2018-12-17 17:06:37 +0000

    updated delete method to more RESTful layout

--- todo_controller.rb
+++ todo_controller.rb
@@ -3,28 +3,30 @@ require 'sinatra/reloader'
 require '../Todo_list/index_page'
 gem 'rack-test'
 require 'logger'
+use Rack::MethodOverride
+
 
 set :logger, Logger.new(STDOUT)
 
-use_list = ManageDatabase.new
+database = ManageDatabase.new
 
 get '/' do
-  items = use_list.view_full_list
+  items = database.view_full_list
   message = ""
 
   erb :index, :locals => {:items => items, :message => message} #:result => result}
 end
 
-get '/items' do # retrieve a list of resources
-
-end
-
-get '/items/:id' do # retrieve a single record
-  jbjb
-end
+# get '/items' do # retrieve a list of resources
+#   logger.info params
+# end
+#
+# get '/items/:id' do # retrieve a single record
+#
+# end
 
 post '/items' do # create a new record via the post method
-  use_list.add_item(params['description'])
+  database.add_item(params['description'])
   redirect to("/")
 
   # item = ManageDatabase.new params["description"]
@@ -39,49 +41,8 @@ put '/items/:id' do # update an existing record
 
 end
 
-delete '/items/:id' do # destroy a record using delete
-  item = ManageDatabase.get params[:id]
-  if item.destroy
-
-
-  use_list.user_deletes(params["id"])
+delete '/items' do # destroy a record using delete
+  logger.info params
+  database.delete_item(params["id"])
   redirect to("/")
-  end
-end
-
-get '/edit_item' do
-  if params["find"] && params["edit"]
-    find_item = params["find"]
-    change_to = params["edit"]
-    use_list.edit_item(find_item, change_to)
-    message = "changed to #{change_to}"
-  end
-
-  erb :index, :locals => {:display_list => display_list, :message => message} #:result => result}
-end
-
-get '/mark_complete' do
-  if params["complete"]
-    change_status = params["complete"]
-    use_list.mark_complete(change_status)
-    message = "#{change_status} has been marked as complete"
-  end
-
-  erb :index, :locals => {:display_list => display_list, :message => message} #:result => result}
-end
-
-get '/view_active' do
-  if params["display_active"]
-    display_list = use_list.view_active_list
-  end
-
-  erb :index, :locals => {:display_list => display_list, :message => message} #:result => result}
-end
-
-get '/view_complete' do
-  if params["display_complete"]
-    display_list = use_list.view_completed_list
-  end
-
-  erb :index, :locals => {:display_list => display_list, :message => message} #:result => result}
 end
